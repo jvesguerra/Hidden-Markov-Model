9542ab43c0696bf7919f1c7b646ff6972c438647
jvesguerra <jvesguerra@up.edu.ph>
2022-11-29 20:18:46 +0800

    Feat: Partial Completion Part 2

--- a.py
+++ a.py
@@ -43,9 +43,9 @@ prob_dict[key[1]] = 0           #T
 #string = data[1]
 string = "STSSTSTSSSTT"
 if string[0] == key[0]:
-    prob_dict[key[0]+"0"] = 1
+    prob_dict[key[0]] = 1
 else:
-    prob_dict[key[1]+"0"] = 1
+    prob_dict[key[1]] = 1
 
 s_total = 0
 t_total = 0
@@ -63,7 +63,7 @@ for i in range(0,string_len):
         if string[i+1] == key[0]: # if next char is also S
             ss_total += 1
 
-prob_dict[key[0]+key[0]+"0"] = ss_total/s_total
+prob_dict[key[0]+key[0]] = ss_total/s_total
 
 # S with next state T
 ts_total = 0
@@ -72,7 +72,7 @@ for i in range(0,string_len-1):
         if string[i+1] == key[1]: # if next char is T
             ts_total += 1
 
-prob_dict[key[1]+key[0]+"0"] = ts_total/s_total
+prob_dict[key[1]+key[0]] = ts_total/s_total
 t_total = ts_total
 
 # T with next state T
@@ -82,7 +82,7 @@ for i in range(0,string_len-1):
         if string[i+1] == key[1]: # if next char is T
             tt_total += 1
 
-prob_dict[key[1]+key[1]+"0"] = tt_total/t_total
+prob_dict[key[1]+key[1]] = tt_total/t_total
 
 # T with next state S
 st_total = 0
@@ -91,9 +91,9 @@ for i in range(0,string_len-1):
         if string[i+1] == key[0]: # if next char is S
             st_total += 1
 
-prob_dict[key[0]+key[1]+"0"] = st_total/t_total
+prob_dict[key[0]+key[1]] = st_total/t_total
 
-print(prob_dict)
+#print(prob_dict)
 
 # END PART 1.
 
@@ -102,7 +102,7 @@ print(prob_dict)
 # formula for total probability
 
 # check array of what to get
-test_string = "S3"
+test_string = "T3"
 
 char1 = key[0]+"1"
 char2 = key[1]+"1"
@@ -111,23 +111,30 @@ check_num = int(test_string[1])
 
 for i in range(0,check_num):
     if i == 0:
-        if check_let == key[0]:
-            s_Total_prob = prob_dict.get("SS")*prob_dict.get("S") + prob_dict.get("ST")*prob_dict.get("T")
-            prob_dict["S1"] = s_Total_prob
-        elif check_let == key[1]:
-            t_Total_prob = prob_dict.get("TS")*prob_dict.get("S") + prob_dict.get("TT")*prob_dict.get("T")
-            prob_dict["T1"] = t_Total_prob
+        s_Total_prob = prob_dict.get("SS")*prob_dict.get("S") + prob_dict.get("ST")*prob_dict.get("T")
+        prob_dict["S1"] = s_Total_prob
+        t_Total_prob = prob_dict.get("TS")*prob_dict.get("S") + prob_dict.get("TT")*prob_dict.get("T")
+        prob_dict["T1"] = t_Total_prob
     else:
-        # FOR S
-        if check_let == key[0]:
-            key1 = key[0]+ str(i)
-            prev_key1 = key[0]+ str(i-1)
-            print(prev_key1)
+        key1 = key[0]+ str(i+1)  # for saving in dictionary
+        key2 = key[1]+ str(i+1)  # for saving in dictionary
 
-        # FOR T
-        elif check_let == key[1]:
-            key2 = key[1]+ str(i)
-            prev_key2 = key[1]+ str(i-1)
+        # values
+        Sn = key[0]+ str(i)
+        Tn = key[1]+ str(i)
+        # print(prob_dict.get(Sn))
+        # print(prob_dict.get(Tn))
+
+        # solve T
+        t_Total_prob = prob_dict.get(Tn)*prob_dict.get(Sn)+prob_dict.get("TT")*prob_dict.get(Tn)
+        prob_dict[key2] = t_Total_prob
+        #solve S
+        s_Total_prob = prob_dict.get("SS")*prob_dict.get(Sn) + prob_dict.get("ST")*prob_dict.get(Tn)
+        prob_dict[key1] = s_Total_prob
+
+print(prob_dict)
+
+# END OF PART 2
 
         
 
